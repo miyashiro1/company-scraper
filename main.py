5c40d053bc2f95ff899a5ad0207ee579fa179e2f
Marcos Miyashiro <58271736+miyashiro1@users.noreply.github.com>
2022-10-08 13:52:06 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import json
+import csv
+
+with open('data2.json') as f:
+    data = json.load(f)
+
+# Create a csv files
+with open('freework.csv', 'a', encoding="utf-8") as new_file:
+    fieldnames = ['name', 'presentation', 'année de création', 'Chiffre d’affaires', 'Collaborateurs',
+                  'siége', 'Activité', 'website', 'twitter', 'facebook', 'linkedin', 'skills']
+    csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter=';')
+    csv_writer.writeheader()
+
+    for i in data['hydra:member']:
+        r = ''
+        lista_skills =[]
+        try:
+            name = i['name']
+            print(name)
+            presentation = i['description']
+            creation = i['creationYear']
+            revenue = i['annualRevenue']
+            Collaborateurs = i["size"]["label"]
+            location = i['location']['label']
+            activity = i["businessActivity"]["name"]
+            website = i["websiteUrl"]
+            twitter = i["twitterUrl"]
+            facebook = i["facebookUrl"]
+            linkedin = i["linkedInUrl"]
+            for s in i['skills']:
+                lista_skills.append(s['name'])
+        except:
+            continue
+
+        skill = ', '.join(lista_skills)
+
+        csv_writer.writerow({'name': name, 'presentation': presentation, 'année de création': creation,
+                             'Chiffre d’affaires': revenue, 'Collaborateurs': Collaborateurs, 'siége': location,
+                             'Activité': activity, 'website': website, 'twitter': twitter, 'facebook': facebook,
+                             'linkedin': linkedin, 'skills': skill})
+
+
+
